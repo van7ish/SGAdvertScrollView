ce612f23134dcb4618eb346f03e654bb89581ec4
van7ish <van7ish@gmail.com>
2017-11-09 16:16:09 +0800

    去掉 IBInspectable

--- SGAdvertScrollView/SGAdvertScrollView.h
+++ SGAdvertScrollView/SGAdvertScrollView.h
@@ -29,11 +29,11 @@ typedef NS_ENUM(NSUInteger, SGAdvertScrollViewStyle) {
 /** delegate */
 @property (nonatomic, weak) id<SGAdvertScrollViewDelegate> delegate;
 /** 默认 SGAdvertScrollViewStyleNormal 样式 */
-@property (nonatomic, assign) IBInspectable SGAdvertScrollViewStyle advertScrollViewStyle;
+@property (nonatomic, assign) SGAdvertScrollViewStyle advertScrollViewStyle;
 /** 滚动时间间隔，默认为3s */
-@property (nonatomic, assign) IBInspectable CFTimeInterval scrollTimeInterval;
+@property (nonatomic, assign) CFTimeInterval scrollTimeInterval;
 /** 标题字体字号，默认为13号字体 */
-@property (nonatomic, strong) IBInspectable UIFont *titleFont;
+@property (nonatomic, strong) UIFont *titleFont;
 
 #pragma mark - - - SGAdvertScrollViewStyleNormal 样式下的 API
 /** 左边标志图片数组 */
@@ -41,9 +41,9 @@ typedef NS_ENUM(NSUInteger, SGAdvertScrollViewStyle) {
 /** 标题数组 */
 @property (nonatomic, strong) NSArray *titles;
 /** 标题字体颜色，默认为黑色 */
-@property (nonatomic, strong) IBInspectable UIColor *titleColor;
+@property (nonatomic, strong) UIColor *titleColor;
 /** 标题文字位置，默认为 NSTextAlignmentLeft，仅仅针对标题起作用 */
-@property (nonatomic, assign) IBInspectable NSTextAlignment textAlignment;
+@property (nonatomic, assign) NSTextAlignment textAlignment;
 
 #pragma mark - - - SGAdvertScrollViewStyleMore 样式下的 API
 /** 顶部左边标志图片数组 */
@@ -55,8 +55,8 @@ typedef NS_ENUM(NSUInteger, SGAdvertScrollViewStyle) {
 /** 底部标题数组 */
 @property (nonatomic, strong) NSArray *bottomTitles;
 /** 顶部标题字体颜色，默认为黑色 */
-@property (nonatomic, strong) IBInspectable UIColor *topTitleColor;
+@property (nonatomic, strong) UIColor *topTitleColor;
 /** 底部标题字体颜色，默认为黑色 */
-@property (nonatomic, strong) IBInspectable UIColor *bottomTitleColor;
+@property (nonatomic, strong) UIColor *bottomTitleColor;
 
 @end
